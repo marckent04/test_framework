0e1afba27719b113005f3c8bcd1233a70f08cfbe
fullstackdev225 <marchenrynanguy1@gmail.com>
2024-09-30 21:43:54 +0200

    feat(frontend): simplify sentences wildcard writing

--- frontend/form/i_fill_input_with.go
+++ frontend/form/i_fill_input_with.go
@@ -5,7 +5,7 @@ import (
 )
 
 var iFillTheInputWith = common.FrontStep{
-	Sentences: []string{`^I fill the ([^"]*) input with "([^"]*)"$`},
+	Sentences: []string{`^I fill the {string} input with "{string}"$`},
 	Definition: func(ctx *common.Context) common.FrontStepDefinition {
 		return func(inputLabel, value string) error {
 			input := common.GetInputElement(ctx.GetCurrentPage(), inputLabel)
