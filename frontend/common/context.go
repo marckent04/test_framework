d79944ab018732c8734d0efbf5f2b71a0803fad2
fullstackdev225 <marchenrynanguy1@gmail.com>
2024-10-06 16:21:25 +0200

    refactor: browser driver abstraction

--- frontend/common/context.go
+++ frontend/common/context.go
@@ -1,26 +1,28 @@
 package common
 
 import (
-	"github.com/go-rod/rod"
+	"cucumber/frontend/common/browser"
 )
 
 type Context struct {
-	browser *rod.Browser
-	page    *rod.Page
+	browser browser.Browser
+	page    browser.Page
 }
 
 func (fc *Context) InitBrowser() {
-	fc.browser = CreateInstance()
+	fc.browser = browser.CreateInstance()
 }
 
 func (fc *Context) OpenNewPage(url string) {
-	fc.page = fc.browser.MustPage(url)
-	fc.page.MustWaitNavigation()
-	fc.page.MustWaitIdle()
+	fc.page = fc.browser.NewPage(url)
 }
 
-func (fc *Context) GetCurrentPage() *rod.Page {
+func (fc *Context) GetCurrentPage() browser.Page {
 	return fc.page
+}
+
+func (fc *Context) GetCurrentPageKeyboard() browser.Keyboard {
+	return fc.page.GetKeyboard()
 }
 
 func NewFrontendContext() *Context {
