13becc8560d68be6bc9047b40e89cd660b077082
fullstackdev225 <marchenrynanguy1@gmail.com>
2024-10-14 00:55:23 +0200

    feat: run cucumber tests by main.go

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"cucumber/frontend"
+	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
+	"log"
+	"os"
+)
+
 func main() {
+
+	var opts = godog.Options{
+		Output:      colors.Colored(os.Stdout),
+		Concurrency: 2,
+		Format:      "pretty",
+		Paths:       []string{"features"},
+	}
+
+	testSuite := godog.TestSuite{
+		Name:    "App",
+		Options: &opts,
+		ScenarioInitializer: func(context *godog.ScenarioContext) {
+			frontend.InitializeScenario(context)
+		},
+	}
+
+	status := testSuite.Run()
+
+	if status != 0 {
+		log.Fatalf("zero status code expected, %d received", status)
+	}
 
 }
