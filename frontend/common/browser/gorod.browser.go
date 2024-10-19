cc7f6cef7e42aaef35ad8a5168eaf74c9051eb6f
fullstackdev225 <marchenrynanguy1@gmail.com>
2024-10-19 16:58:13 +0200

    feat(browser): add actions timeout

--- frontend/common/browser/gorod.browser.go
+++ frontend/common/browser/gorod.browser.go
@@ -1,10 +1,10 @@
 package browser
 
 import (
-	"os"
-
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
+	"os"
+	"time"
 )
 
 type rodBrowser struct {
@@ -26,12 +26,13 @@ func newRodBrowser() Browser {
 }
 
 func instantiateRodBrowser() *rod.Browser {
+	headlessMode := os.Getenv("DEBUG") != "true"
 	path, _ := launcher.LookPath()
-	if os.Getenv("DEBUG") == "true" {
-		u := launcher.New().Bin(path).
-			Headless(false).
-			MustLaunch()
-		return rod.New().ControlURL(u).MustConnect()
-	}
-	return rod.New().ControlURL(launcher.New().Bin(path).MustLaunch()).MustConnect()
+	u := launcher.New().Bin(path).
+		Headless(headlessMode).
+		MustLaunch()
+
+	const seconds = 10
+	return rod.New().ControlURL(u).MustConnect().Timeout(seconds * time.Second)
+
 }
