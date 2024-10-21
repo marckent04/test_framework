7559020aff71db81e0556f42297e86c9badbc289
marckent04 <marchenrynanguy1@gmail.com>
2024-10-21 16:24:09 +0200

    feat(report): generate tests suite report

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/cucumber/godog v0.14.1
 	github.com/go-rod/rod v0.116.2
 	github.com/goccy/go-yaml v1.12.0
+	github.com/tdewolff/minify/v2 v2.21.0
+	github.com/tdewolff/parse v2.3.4+incompatible
 )
 
 require (
@@ -20,6 +22,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tdewolff/parse/v2 v2.7.18 // indirect
 	github.com/ysmood/fetchup v0.2.3 // indirect
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/got v0.40.0 // indirect
--- main.go
+++ main.go
@@ -1,34 +1,73 @@
 package main
 
 import (
+	"context"
 	"cucumber/frontend"
+	"cucumber/report"
 	"log"
-	"os"
 
 	"github.com/cucumber/godog"
-	"github.com/cucumber/godog/colors"
+	"github.com/tdewolff/parse/buffer"
 )
 
 func main() {
 	const concurrency = 2
 	var opts = godog.Options{
-		Output:      colors.Colored(os.Stdout),
-		Concurrency: concurrency,
-		Format:      "pretty",
-		Paths:       []string{"features"},
+		Output:              &buffer.Writer{},
+		Concurrency:         concurrency,
+		Format:              "pretty",
+		ShowStepDefinitions: false,
+		Paths:               []string{"features"},
 	}
 
+	testReport := report.New()
+
 	testSuite := godog.TestSuite{
-		Name:    "App",
-		Options: &opts,
-		ScenarioInitializer: func(context *godog.ScenarioContext) {
-			frontend.InitializeScenario(context)
-		},
+		Name:                 "App",
+		Options:              &opts,
+		TestSuiteInitializer: testSuiteInitializer(&testReport),
+		ScenarioInitializer:  scenarioInitializer(&testReport),
 	}
 
 	status := testSuite.Run()
 
 	if status != 0 {
 		log.Fatalf("zero status code expected, %d received", status)
+	}
+}
+
+func testSuiteInitializer(testReport *report.Report) func(*godog.TestSuiteContext) {
+	return func(suiteContext *godog.TestSuiteContext) {
+		suiteContext.BeforeSuite(func() {
+			testReport.Start()
+		})
+
+		suiteContext.AfterSuite(func() {
+			testReport.Write()
+		})
+	}
+}
+func scenarioInitializer(testReport *report.Report) func(*godog.ScenarioContext) {
+	return func(sc *godog.ScenarioContext) {
+		frontend.InitializeScenario(sc)
+		scenarioReport := report.NewScenario()
+
+		sc.StepContext().After(afterStepHookInitializer(&scenarioReport))
+		sc.After(afterScenarioHookInitializer(testReport, &scenarioReport))
+	}
+}
+func afterStepHookInitializer(scenarioReport *report.Scenario) godog.AfterStepHook {
+	return func(ctx context.Context, st *godog.Step, status godog.StepResultStatus, err error) (context.Context, error) {
+		scenarioReport.AddStep(st.Text, status, err)
+		return ctx, err
+	}
+}
+func afterScenarioHookInitializer(testReport *report.Report, scenarioReport *report.Scenario) godog.AfterScenarioHook {
+	return func(ctx context.Context, sc *godog.Scenario, err error) (context.Context, error) {
+		scenarioReport.SetTitle(sc.Name)
+
+		scenarioReport.End()
+		testReport.AddScenario(*scenarioReport)
+		return ctx, err
 	}
 }
