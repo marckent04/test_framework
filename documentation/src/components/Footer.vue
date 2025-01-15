6f6278deb7195f8b44e95c770c9b710ff3cd8cbc
Marc-Henry Nanguy <36090455+marckent04@users.noreply.github.com>
2025-01-15 13:15:59 +0100

    Revert "Docs/app"

--- documentation/src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <footer class="bg-gray-800 py-4 text-center text-white">
-        <p>&copy; 2023 - EToolsE</p>
-    </footer>
-</template>
--- documentation/src/views/index.ts
+++ documentation/src/views/index.ts
@@ -5,37 +5,13 @@ import {
 } from "vue-router";
 
 import HomeView from "./HomeView.vue";
-import SentencesView from "./SentencesView.vue";
-import ConfigurationView from "./ConfigurationView.vue";
-import QuickStartView from "./QuickStartView.vue";
-import GetStartedView from "./GetStartedView.vue";
 
 export const routeNames = {
   home: "home",
-  getStarted: "get-started",
-  quickstart: "quickstart",
-  sentences: "sentences",
-  config: "config",
 };
 
 const routes: readonly RouteRecordRaw[] = [
   { path: "/", component: HomeView, name: routeNames.home },
-  {
-    path: "/get-started",
-    component: GetStartedView,
-    name: routeNames.getStarted,
-  },
-  {
-    path: "/quick-start",
-    component: QuickStartView,
-    name: routeNames.quickstart,
-  },
-  { path: "/sentences", component: SentencesView, name: routeNames.sentences },
-  {
-    path: "/configuration",
-    component: ConfigurationView,
-    name: routeNames.config,
-  },
 ];
 
 export const router = createRouter({
